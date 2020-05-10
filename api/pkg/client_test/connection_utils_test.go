443f7fc8d733e49efb0def128e80e28ab072e92b
Steven Gantz <steven.p.gantz@gmail.com>
2020-05-10 15:59:05 -0400

    Added first successful unit test

--- api/pkg/client_test/connection_utils_test.go
+++ api/pkg/client_test/connection_utils_test.go
@@ -2,10 +2,17 @@ package client_test
 
 import (
 	"fmt"
+	"github.com/stevenpg/kubutilization/api/pkg/client"
+	"github.com/stretchr/testify/assert"
+	"os"
 	"testing"
 )
 
 // TestHomeDir - test homedir function for returning variable value
 func TestHomeDir(t *testing.T) {
-	fmt.Println("Run test")
+
+	assert := assert.New(t)
+	homeDir := client.HomeDir()
+	fmt.Printf("Retrieving %s\n", homeDir)
+	assert.True(homeDir == os.Getenv("USERPROFILE") || homeDir == os.Getenv("HOME"))
 }
--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/golang/protobuf v1.4.1 // indirect
 	github.com/googleapis/gnostic v0.4.1 // indirect
 	github.com/imdario/mergo v0.3.9 // indirect
+	github.com/stretchr/testify v1.4.0
 	golang.org/x/crypto v0.0.0-20200429183012-4b2356b1ed79 // indirect
 	golang.org/x/net v0.0.0-20200506145744-7e3656a0809f // indirect
 	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d // indirect
