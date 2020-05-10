2c0372fef8d979681a4d0c6821a806dcc43469be
Steven Gantz <steven.p.gantz@gmail.com>
2020-05-10 16:31:34 -0400

    Add coverage scripts, removed some testing components

--- api/cmd/api.go
+++ api/cmd/api.go
@@ -5,16 +5,15 @@ import (
 	"github.com/stevenpg/kubutilization/api/pkg/client"
 )
 
-// initFlags - Initializes are parses flags, returning the ptrs to caller
-func initFlags() *bool {
-	ExternalKubernetesPtr := flag.Bool("external", false, "a boolean selector for whether to search for a kubeconfig in ~/.kube/config")
+// parseFlagUseExternalConnection - Initializes are parses flags, returning the ptrs to caller
+func parseFlagUseExternalConnection() *bool {
+	externalConnection := flag.Bool("external", false, "a boolean selector for whether to search for a kubeconfig in ~/.kube/config")
 	flag.Parse()
-
-	return ExternalKubernetesPtr
+	return externalConnection
 }
 
 func main() {
-	if *initFlags() {
+	if *parseFlagUseExternalConnection() {
 		client.ExternalConnection()
 	} else {
 		client.Connection()
--- api/test/run-all-tests.ps1
+++ api/test/run-all-tests.ps1
@@ -1,2 +1,2 @@
-go test -v ../...
+go test -v ../... -coverprofile cp.out
 # TODO - ignore output from all lines that have [no test files] in it
