002f1fd028f8cb9dcb66a3e237b0a38d9561345a
StevenPG <steven.p.gantz@gmail.com>
2020-05-12 23:18:48 -0400

    Add node endpoints

--- api/pkg/endpoint/node_endpoints.go
+++ api/pkg/endpoint/node_endpoints.go
@@ -1,22 +1,28 @@
 package endpoint
 
 import (
+	"context"
 	"github.com/gin-gonic/gin"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
 	"net/http"
 )
 
 // GetNodes ... returns basic node data in JSON format
 func GetNodes(c *gin.Context) {
+	connection := c.MustGet("clientset").(kubernetes.Clientset)
+	namespaces, _ := connection.CoreV1().Namespaces().List(context.TODO(), metav1.ListOptions{})
 	c.JSON(http.StatusOK, gin.H{
-		"Status": "OK",
+		"result": namespaces,
 	})
 }
 
 // GetNode ... get basic node data from input parameter
 func GetNode(c *gin.Context) {
 	node := c.Param("node")
+	connection := c.MustGet("clientset").(kubernetes.Clientset)
+	namespaces, _ := connection.CoreV1().Namespaces().Get(context.TODO(), node, metav1.GetOptions{})
 	c.JSON(http.StatusOK, gin.H{
-		"Status":       "Ok",
-		"selectedNode": node,
+		"result": namespaces,
 	})
 }
