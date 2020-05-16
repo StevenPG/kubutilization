41f04cdbe580357b9cb94e81984091ba6fcffc2a
StevenPG <steven.p.gantz@gmail.com>
2020-05-16 12:10:51 -0400

    Added newman collection and updated endpoint to pull correct data

--- api/pkg/endpoint/node_endpoints.go
+++ api/pkg/endpoint/node_endpoints.go
@@ -11,18 +11,18 @@ import (
 // GetNodes ... returns basic node data in JSON format
 func GetNodes(c *gin.Context) {
 	connection := c.MustGet("clientset").(kubernetes.Clientset)
-	namespaces, _ := connection.CoreV1().Namespaces().List(context.TODO(), metav1.ListOptions{})
+	nodes, _ := connection.CoreV1().Nodes().List(context.TODO(), metav1.ListOptions{})
 	c.JSON(http.StatusOK, gin.H{
-		"result": namespaces,
+		"result": nodes,
 	})
 }
 
 // GetNode ... get basic node data from input parameter
 func GetNode(c *gin.Context) {
-	node := c.Param("node")
+	nodeParam := c.Param("node")
 	connection := c.MustGet("clientset").(kubernetes.Clientset)
-	namespaces, _ := connection.CoreV1().Namespaces().Get(context.TODO(), node, metav1.GetOptions{})
+	node, _ := connection.CoreV1().Nodes().Get(context.TODO(), nodeParam, metav1.GetOptions{})
 	c.JSON(http.StatusOK, gin.H{
-		"result": namespaces,
+		"result": node,
 	})
 }
